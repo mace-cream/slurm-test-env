4f569d794e73bd97679fac588d5554d70d340a16
zhaofeng-shu33 <616545598@qq.com>
2019-11-01 15:54:47 +0800

    update slurm conf on compute node

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget http://10.8.4.170:88/zhaofeng-shu33/slurm-test-env/raw/master/slurm.conf?inline=false -O slurm.conf
+if [[ "$(hostname)" = "raspberrypi" ]]; then 
+	sudo mv ./slurm.conf /etc/
+else
+	sudo mv ./slurm.conf /etc/slurm/
+fi
+
