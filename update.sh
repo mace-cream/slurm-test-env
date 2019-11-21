e06cd73b9122a1b0660df7cc2eda00bf5fe79655
zhaofeng <616545598@qq.com>
2019-11-21 21:31:48 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 wget http://10.8.4.170:88/zhaofeng-shu33/slurm-test-env/raw/master/slurm.conf?inline=false -O slurm.conf
-if [[ "$(hostname)" = "zhaofengLaptop" ]]; then 
-	sudo mv ./slurm.conf /etc/slurm/
-else
-	sudo mv ./slurm.conf /etc/slurm-llnl/
-fi
+sudo mv ./slurm.conf /etc/slurm-llnl/
 
