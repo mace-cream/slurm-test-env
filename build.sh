86d8288f3b4b865fae7c845b55b05699c88498f9
zhaofeng-shu33 <616545598@qq.com>
2020-04-30 21:46:22 +0800

    show the packages exist

--- build.sh
+++ build.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 set -e -x
 echo "hello world"
-cd ~/slurm-llnl-18.08.5.2
+cd ~/
+# remove old deb file
+find ./ -maxdepth 1 -name "*.deb" | grep 18.08.5.2 | xargs rm -f
+cd slurm-llnl-18.08.5.2
 # https://gitee.com/freewind201301/slurm-llnl-18.08
 git rev-parse --verify HEAD
 dpkg-buildpackage -uc -us
-
+cd ..
+# list artifact
+find ./ -maxdepth 1 -name "*.deb" | grep 18.08.5.2
+# Raspbian 8 uses debian jessie, whose gcc version is 4.9.
+# GitHub action bundles a higher version of node
+# which the default system libstdc++ does not support.
+# Therefore it is impossible to use @actions/xxx, which
+# uses node to execute JS code.
+# Theoreticially it is possible to hard coded the upload
+# code using the restful api.
+# See https://github.com/actions/toolkit/blob/57d20b4db494c25af8d2f3d9323650044610e531/packages/artifact/src/internal/utils.ts#L216
+# But it may change since it is internally used by GitHub.
+# Manually uploaded artifect can be found at
+# https://bintray.com/zhaofeng-shu33/raspbian8/slurm/18.08
